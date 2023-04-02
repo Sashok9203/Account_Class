8112534a92409d1ea806d0705c1b2ce9a278289c
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-04-02 22:43:21 +0300

    Добавьте файлы проекта.

---
+++ Account_Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Account_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
